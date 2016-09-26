078d2e51a7bbb2c77438fe8bb3f0c68d1b444f2b
cassiandrei <cschneider@inf.ufsm.br>
2016-09-26 04:28:33 -0300

    questoes activity

---
+++ app/src/main/java/compclub/inf/com/logicinalogicway/QuestoesActivity.java
@@ -0,0 +1,13 @@
+package compclub.inf.com.logicinalogicway;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class QuestoesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_questoes);
+    }
+}
